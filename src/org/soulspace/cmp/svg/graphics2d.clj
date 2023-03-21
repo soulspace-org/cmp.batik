9ee638e53abf6b4d23a6e264f70415cd811d326c
Ludger Solbach <soulman@soulspace.org>
2023-03-21 15:27:50 +0100

    updated Batik to 1.16, added comments

--- src/org/soulspace/cmp/svg/graphics2d.clj
+++ src/org/soulspace/cmp/svg/graphics2d.clj
@@ -1,12 +1,14 @@
-;;
-;;   Copyright (c) Ludger Solbach. All rights reserved.
-;;   The use and distribution terms for this software are covered by the
-;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;;   which can be found in the file license.txt at the root of this distribution.
-;;   By using this software in any fashion, you are agreeing to be bound by
-;;   the terms of this license.
-;;   You must not remove this notice, or any other, from this software.
-;;
+;;;;
+;;;;   Copyright (c) Ludger Solbach. All rights reserved.
+;;;;
+;;;;   The use and distribution terms for this software are covered by the
+;;;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;;;   which can be found in the file license.txt at the root of this distribution.
+;;;;   By using this software in any fashion, you are agreeing to be bound by
+;;;;   the terms of this license.
+;;;;
+;;;;   You must not remove this notice, or any other, from this software.
+;;;;
 
 (ns org.soulspace.cmp.svg.graphics2d
   (:require [clojure.java.io :as io])
@@ -14,6 +16,11 @@
            [java.awt.geom Rectangle2D]
            [org.apache.batik.dom GenericDOMImplementation]
            [org.apache.batik.svggen SVGGraphics2D]))
+
+
+;;;
+;;; SVG rendering with Graphics2D for pixel file output
+;;;
 
 (defn rectangle-2d [width height]
   (java.awt.geom.Rectangle2D$Double. 0 0 width height))
